e741621696eb5d11f8cc0196606286f88d03aeba
NinoosMoshi <nalkaton@gmail.com>
2021-06-28 08:07:19 -0400

    create spring project

---
+++ backEnd/ishtar-spring/src/main/java/com/ninos/IshtarSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ninos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshtarSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshtarSpringApplication.class, args);
+	}
+
+}
