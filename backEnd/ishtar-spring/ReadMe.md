0ee4bc9af56f830f8ba852cd89194c7830cc01e6
NinoosMoshi <nalkaton@gmail.com>
2021-06-30 09:05:18 -0400

    Category,Order service #5

--- backEnd/ishtar-spring/ReadMe.md
+++ backEnd/ishtar-spring/ReadMe.md
@@ -1,5 +1,6 @@
 1- create Category and Order class,
    relationship is : one Category has many orders,and many orders have one category.
 
-2- application.properties
-3- create Category and Order repository
+2- application.properties</br>
+3- create Category and Order repository</br>
+4- create Category and Order service</br>
---
+++ backEnd/ishtar-spring/src/main/java/com/ninos/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.ninos.service;
+
+import com.ninos.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+
+}
