8801e3eb7ffefb644970d9c170cc36f29b764ffc
NinoosMoshi <nalkaton@gmail.com>
2021-07-01 06:26:17 -0400

    Angular create OrderService #11

---
+++ frontEnd/ishtar-angular/src/app/services/order.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from '../model/order';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  private baseUrl = "http://localhost:8080/api";
+
+  constructor(private http: HttpClient) { }
+
+  // http://localhost:8080/api/allOrders
+  getAllOrders(): Observable<Order[]>{
+   return this.http.get<Order[]>(`${this.baseUrl}/allOrders`).pipe(
+     map( response => response)
+   );
+  }
+
+
+}
