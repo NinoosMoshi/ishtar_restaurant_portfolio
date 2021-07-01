5929642c07ce21103aa8d1a81ea9b91e6427ac84
NinoosMoshi <nalkaton@gmail.com>
2021-07-01 07:01:05 -0400

    Angular create order component #12

---
+++ frontEnd/ishtar-angular/src/app/components/order-list/order-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/model/order';
+import { OrderService } from 'src/app/services/order.service';
+
+@Component({
+  selector: 'app-order-list',
+  templateUrl: './order-list.component.html',
+  styleUrls: ['./order-list.component.css']
+})
+export class OrderListComponent implements OnInit {
+
+  orders: Order[]=[];
+
+  constructor(private orderService: OrderService) { }
+
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+
+  getOrders(){
+    this.orderService.getAllOrders().subscribe(data =>{
+      this.orders = data;
+    })
+  }
+
+
+
+}
