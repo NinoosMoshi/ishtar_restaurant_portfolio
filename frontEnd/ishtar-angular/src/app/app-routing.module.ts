0f1437352a27fe434e1338bc3cf88b18cf2be4be
NinoosMoshi <nalkaton@gmail.com>
2021-07-02 23:25:13 -0400

    Angular create routes #16

--- frontEnd/ishtar-angular/src/app/app-routing.module.ts
+++ frontEnd/ishtar-angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { OrderListComponent } from './components/order-list/order-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'category/:id', component: OrderListComponent},     // http://localhost:4200/category/{id}
+  {path:'category', component: OrderListComponent},         // http://localhost:4200/category
+  {path:'orders', component: OrderListComponent},           // http://localhost:4200/orders
+  {path:'', redirectTo:'/orders',pathMatch:'full'},         // http://localhost:4200
+  {path:'**', redirectTo:'/orders',pathMatch:'full'},       // http://localhost:4200/jsdkljkd
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
