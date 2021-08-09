7084bfdc0188aadc3fde2907dd98ca4c8fee7fa6
NinoosMoshi <nalkaton@gmail.com>
2021-08-09 10:50:52 -0400

    Angular resetNewPassword Closes #96

--- frontEnd/ishtar-angular/src/app/components/reset-password/reset-password.component.ts
+++ frontEnd/ishtar-angular/src/app/components/reset-password/reset-password.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/security/authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { SpaceValidator } from 'src/app/model/space-validator';
@@ -11,9 +13,9 @@ export class ResetPasswordComponent implements OnInit {
 
   formParentGroup!: FormGroup;
   formParentGroupReset!: FormGroup;
-  enableFrom: boolean = false;
+  enableFrom: boolean = true;
 
-  constructor(private formChildGroup: FormBuilder) { }
+  constructor(private formChildGroup: FormBuilder,private authenticationService: AuthenticationService, private router:Router) { }
 
   ngOnInit(): void {
     this.mySignupForm();
@@ -23,11 +25,12 @@ export class ResetPasswordComponent implements OnInit {
 
   mySignupForm(){
     this.formParentGroup = this.formChildGroup.group({
-      user: this.formChildGroup.group({
-        email: new FormControl('',[Validators.required,
-                                   SpaceValidator.onlyContainSpace,
-                                   Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$') ]),
-        password: new FormControl('', [Validators.required])
+      user:this.formChildGroup.group({
+        email: new FormControl('',[
+          Validators.required,
+          SpaceValidator.onlyContainSpace,
+          Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')
+        ])
       })
     })
   }
@@ -63,8 +66,18 @@ export class ResetPasswordComponent implements OnInit {
   done(){
     if(this.formParentGroup.invalid){
       this.formParentGroup.markAllAsTouched();
-      return;
-    }
+      return
+   }
+    this.authenticationService.checkEmail(this.formParentGroup.controls['user'].value.email).subscribe({
+      next: response =>{
+        if(response.result == 1){
+           this.enableFrom = false;
+        }else{
+           alert("Email doesn't exists")
+        }
+      }
+    })
+
   }
 
 
@@ -72,8 +85,24 @@ export class ResetPasswordComponent implements OnInit {
   resetNewPassword(){
     if(this.formParentGroupReset.invalid){
       this.formParentGroupReset.markAllAsTouched();
-      return;
+      return
     }
+    this.authenticationService.resetPassword(
+      this.formParentGroup.controls['user'].value.email,
+      this.formParentGroupReset.controls['newUser'].value.password,
+      this.formParentGroupReset.controls['newUser'].value.code
+    ).subscribe({
+      next: response =>{
+        if(response.result == 1){
+           alert("Success code")
+           this.router.navigateByUrl("/login")
+        }else{
+           alert("Invalid Code")
+        }
+      }
+    })
+
+
   }
 
 
