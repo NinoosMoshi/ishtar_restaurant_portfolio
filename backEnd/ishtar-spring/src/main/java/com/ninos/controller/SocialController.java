7a2d1649a89bced77b1f5f45914c871064c2c828
NinoosMoshi <nalkaton@gmail.com>
2021-08-11 23:49:46 -0400

    save user sign google in database #100

--- backEnd/ishtar-spring/src/main/java/com/ninos/controller/SocialController.java
+++ backEnd/ishtar-spring/src/main/java/com/ninos/controller/SocialController.java
@@ -5,10 +5,18 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 
+import com.ninos.dto.security.LoginResponse;
 import com.ninos.dto.security.TokenDto;
+import com.ninos.model.security.Authorities;
+import com.ninos.service.security.AuthoritiesService;
+import com.ninos.service.security.UserService;
+import com.ninos.springSecurity.jwt.JwtAuthenticationFilter;
+import com.ninos.springSecurity.jwt.JwtLogin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.User;
 import org.springframework.social.facebook.api.impl.FacebookTemplate;
@@ -19,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Collections;
-
+import java.util.List;
 
 
 @RestController
@@ -29,9 +37,25 @@ public class SocialController {
     @Value("google.id")
     private String idClient;
 
+    @Value("mySecret.password")
+    private String privatePassword;
+
+    private UserService userService;
+    private AuthoritiesService authoritiesService;
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SocialController(UserService userService, AuthoritiesService authoritiesService, JwtAuthenticationFilter jwtAuthenticationFilter, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.authoritiesService = authoritiesService;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     //http://localhost:8080/social/google
     @PostMapping("/google")
-    public ResponseEntity<?> loginWithGoogle(@RequestBody TokenDto tokenDto) throws IOException {
+    public LoginResponse loginWithGoogle(@RequestBody TokenDto tokenDto) throws IOException {
         NetHttpTransport transport = new NetHttpTransport();
         JacksonFactory factory = JacksonFactory.getDefaultInstance();
         GoogleIdTokenVerifier.Builder ver =
@@ -39,7 +63,24 @@ public class SocialController {
                         .setAudience(Collections.singleton(idClient));
         GoogleIdToken googleIdToken = GoogleIdToken.parse(ver.getJsonFactory(),tokenDto.getToken());
         GoogleIdToken.Payload payload = googleIdToken.getPayload();
-        return new ResponseEntity<>(payload, HttpStatus.OK);
+
+        boolean result = userService.emailExists(payload.getEmail());
+        LoginResponse loginResponse;
+        if (!result){
+            com.ninos.model.security.User user = new com.ninos.model.security.User();
+            user.setEmail(payload.getEmail());
+            user.setPassword(passwordEncoder.encode(privatePassword));
+            user.setActive(1);
+            List<Authorities> authorities = authoritiesService.getAllAuthorities();
+            user.getAuthorities().add(authorities.get(0));
+            userService.addUser(user);
+        }
+
+        JwtLogin jwtLogin = new JwtLogin();
+        jwtLogin.setEmail(payload.getEmail());
+        jwtLogin.setPassword(privatePassword);
+        loginResponse = jwtAuthenticationFilter.login(jwtLogin);
+        return loginResponse;
     }
 
 
